e1f33d7f34a2e4e33db0e4a24d3bcc5c5c903d6b
TotallyNotTroy <tjsobrenilla@gmail.com>
2025-12-11 05:50:06 +0800

    Force add environment files for deployment

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+﻿export const environment = {
+    production: false,
+    supabaseUrl: 'https://tnapnzsdrbeaumlvhmws.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuYXBuenNkcmJlYXVtbHZobXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2ODk0NjcsImV4cCI6MjA4MDI2NTQ2N30.0SfnUrAyWqhhrtDDkPFjhoWg30_0q1RvkyduhjaWufw',
+    geminiApiKey: 'AIzaSyDq3r05Kl134qWmJHKyN6iU1WAqkhctA08',
+    backendUrl: 'http://localhost:5000/api',
+    studentApiUrl: 'https://class-list-eight.vercel.app',
+    studentApiKey: '2360351754b04ddc801e0ea0e74d176a35f201ad35dd1100af0bd025a12a91c8'
+};
